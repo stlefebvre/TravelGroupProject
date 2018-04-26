89a1f3d175f06ca1e2fa8b366751d6609d50d10b
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-26 19:27:20 -0400

    created Yelp API variables, and Google API key variable

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    //Javascript goes here
+    //API variables
+    var yelpAPIKey = "qusAduy05LdRpxfqStriSQS46iXpHS1hb_RHHF2HSqlGX5Xw6OMMZqlEkO4C_vb-BlUPl2v-BuP-gy6ek6kXrXCV8LpdQMLaY0wOkC3pFqCjz291AcMEf6hXHVfiWnYx"
+    var yelpClientID = "ZYDpWatxChwhrIdoyZVlFg"
+        //May not need Client ID for Yelp.
+    var yelpURL = "https://api.yelp.com/v3/businesses/search"
+
+    var googleMapAPIKey = "AIzaSyBztLFObJ_vMG1zhWlzys8DWeiONElq2EI"
+
 })
